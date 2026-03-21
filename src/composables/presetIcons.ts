b8371135f1b8ab9880730d586d997dc364513cf0
sifujiang <sifujiang@git.com.cn>
2026-03-21 08:54:22 +0800

    feat: 新增 20 个国内常用网站 SVG 图标
    淘宝/天猫/京东/支付宝/微博/小红书/QQ/网易/美团/携程/饿了么/转转/快手/今日头条/简书/CSDN/掘金/大众点评/闲鱼/知乎(Square)

--- src/composables/presetIcons.ts
+++ src/composables/presetIcons.ts
@@ -13,6 +13,7 @@ export const PRESET_ICONS: PresetIcon[] = [
   { name: 'github', label: 'GitHub', url: 'https://github.com' },
   { name: 'baidu', label: '百度', url: 'https://baidu.com' },
   { name: 'zhihu', label: '知乎', url: 'https://zhihu.com' },
+  { name: 'zhihu-square', label: '知乎', url: 'https://www.zhihu.com' },
   { name: 'douyin', label: '抖音', url: 'https://douyin.com' },
   { name: 'bilibili', label: '哔哩哔哩', url: 'https://bilibili.com' },
   { name: 'douban', label: '豆瓣', url: 'https://douban.com' },
@@ -27,6 +28,9 @@ export const PRESET_ICONS: PresetIcon[] = [
   { name: 'npm', label: 'npm', url: 'https://npmjs.com' },
   { name: 'github', label: 'GitHub', url: 'https://github.com' },
   { name: 'gitlab', label: 'GitLab', url: 'https://gitlab.com' },
+  { name: 'csdn', label: 'CSDN', url: 'https://csdn.net' },
+  { name: 'juejin', label: '掘金', url: 'https://juejin.cn' },
+  { name: 'jianshu', label: '简书', url: 'https://jianshu.com' },
 
   // 前端技术
   { name: 'vue', label: 'Vue', url: 'https://vuejs.org' },
@@ -48,6 +52,24 @@ export const PRESET_ICONS: PresetIcon[] = [
 
   // 通用
   { name: 'browser', label: '浏览器' },
+
+  // 国内常用网站
+  { name: 'taobao', label: '淘宝', url: 'https://taobao.com' },
+  { name: 'tmall', label: '天猫', url: 'https://tmall.com' },
+  { name: 'jd', label: '京东', url: 'https://jd.com' },
+  { name: 'alipay', label: '支付宝', url: 'https://alipay.com' },
+  { name: 'weibo', label: '微博', url: 'https://weibo.com' },
+  { name: 'xiaohongshu', label: '小红书', url: 'https://xiaohongshu.com' },
+  { name: 'qq', label: 'QQ', url: 'https://im.qq.com' },
+  { name: 'wangyi', label: '网易', url: 'https://163.com' },
+  { name: 'meituan', label: '美团', url: 'https://meituan.com' },
+  { name: 'ctrip', label: '携程', url: 'https://ctrip.com' },
+  { name: 'eleme', label: '饿了么', url: 'https://ele.me' },
+  { name: 'zhuanzhuan', label: '转转', url: 'https://zhuanzhuan.com' },
+  { name: 'kuaishou', label: '快手', url: 'https://kuaishou.com' },
+  { name: 'toutiao', label: '今日头条', url: 'https://toutiao.com' },
+  { name: 'dianping', label: '大众点评', url: 'https://dianping.com' },
+  { name: 'xianyu', label: '闲鱼', url: 'https://xianyu.com' },
 ]
 
 /**
